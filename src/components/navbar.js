9b742049e180f52e11b8a40ed4b9ce296ccafa6e
Hung Tang <hung.tang@sjsu.edu>
2019-10-23 19:28:49 -0700

    updatenavbar title

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom';
     render(){
         return(
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <a className="navbar-brand" href="/#">Task Board</a>
+                <a className="navbar-brand" href="/#">Welcome to Task Board</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
